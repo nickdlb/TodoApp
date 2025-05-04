ae66e68436a38b4615a2447d6cebd92eda2a9937
Agencia-DLB <nicolasdiasleote@gmail.com>
2025-05-04 13:44:40 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.tsx",
-    "./components/**/*.tsx",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
